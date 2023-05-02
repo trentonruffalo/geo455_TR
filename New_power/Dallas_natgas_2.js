3226129595d7255e90837d3a3deae439a76ba899
trentonruffalo <123512480+trentonruffalo@users.noreply.github.com>
2023-05-02 15:54:30 -0500

    Add files via upload

---
+++ New_power/Dallas_natgas_2.js
@@ -0,0 +1,173 @@
+var dallasgas = {
+  "type" : "FeatureCollection",
+  "features" : [
+    {
+      "type" : "Feature",
+      "id" : 1,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -96.67500001999997,
+          32.319999950000067
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 1,
+        "Name" : "Ennis",
+        "Location" : "Ellis County",
+        "Coordinate" : "32.3200°N 96.6750°W",
+        "Capacity" : "360",
+        "Generation" : "1x1 Combined Cycle",
+        "Year" : "2002",
+        "Refs" : "[28]",
+        "DDLat" : 32.32,
+        "DDLon" : -96.674999999999997,
+        "ORIG_OID" : 27
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 2,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -96.483333353999967,
+          32.76277772800006
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 2,
+        "Name" : "Forney Energy Center",
+        "Location" : "Kaufman County",
+        "Coordinate" : "32°45′46″N\u00A096°29′00″W",
+        "Capacity" : "1,784.00",
+        "Generation" : "6x2 Combined Cycle",
+        "Year" : "2003",
+        "Refs" : "[35]",
+        "DDLat" : 32.76277778,
+        "DDLon" : -96.483333329999994,
+        "ORIG_OID" : 32
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 3,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -97.219200020999949,
+          32.728299950000064
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 3,
+        "Name" : "Handley",
+        "Location" : "Tarrant County",
+        "Coordinate" : "32.7283°N 97.2192°W",
+        "Capacity" : "1,265.00",
+        "Generation" : "Steam Turbine (x3)",
+        "Year" : "1963/1977",
+        "Refs" : "[27]",
+        "DDLat" : 32.728299999999997,
+        "DDLon" : -97.219200000000001,
+        "ORIG_OID" : 45
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 4,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -96.545800020999991,
+          32.83579995100007
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 4,
+        "Name" : "Lake Hubbard",
+        "Location" : "Dallas County",
+        "Coordinate" : "32.8358°N 96.5458°W",
+        "Capacity" : "915",
+        "Generation" : "Steam Turbine (x2)",
+        "Year" : "1970/1973",
+        "Refs" : "[27]",
+        "DDLat" : 32.835799999999999,
+        "DDLon" : -96.5458,
+        "ORIG_OID" : 59
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 5,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -97.055000019999966,
+          32.429999950000024
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 5,
+        "Name" : "Midlothian",
+        "Location" : "Ellis County",
+        "Coordinate" : "32°25′48″N\u00A097°03′18″W",
+        "Capacity" : "1,734.00",
+        "Generation" : "Combined Cycle (x6)",
+        "Year" : "2000/2001",
+        "Refs" : "[28][41]",
+        "DDLat" : 32.43,
+        "DDLon" : -97.055000000000007,
+        "ORIG_OID" : 64
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 6,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -96.935800019999988,
+          32.723099950000062
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 6,
+        "Name" : "Mountain Creek",
+        "Location" : "Dallas County",
+        "Coordinate" : "32.7231°N 96.9358°W",
+        "Capacity" : "808",
+        "Generation" : "Steam Turbine (x3)",
+        "Year" : "1956/1958/1967",
+        "Refs" : "[27]",
+        "DDLat" : 32.723100000000002,
+        "DDLon" : -96.9358,
+        "ORIG_OID" : 67
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 7,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          -96.452500020999992,
+          33.068099951000079
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 7,
+        "Name" : "Ray Olinger",
+        "Location" : "Collin County",
+        "Coordinate" : "33.0681°N 96.4525°W",
+        "Capacity" : "320",
+        "Generation" : "Steam Turbine (x3)",
+        "Year" : "1967/1971/1976",
+        "Refs" : " ",
+        "DDLat" : 33.068100000000001,
+        "DDLon" : -96.452500000000001,
+        "ORIG_OID" : 87
+      }
+    }
+  ]
+}
